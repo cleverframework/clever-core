144a032d5e6855c831bfc81d499f43e09d0dd2d2
JacopoDaeli <jacopo.daeli@gmail.com>
2015-03-25 13:38:24 +0000

    Implemented onInstanceWaiter

--- private-test/server.js
+++ private-test/server.js
@@ -4,7 +4,7 @@
 let clever = require('../index');
 
 clever.serve((app) => {
-  let config = app.config;
+  let config = app.config.clean;
   let port = config.https && config.https.port ? config.https.port : config.http.port;
   console.log(`Clever app started on port ${port}`);
 });
